2e65c6cae15cc623e32e894241cb0d4836174642
Jelly <littlejellycat@yandex.ru>
2018-02-02 16:17:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Test task
+
+For a given csv-file find the one second-window during which the largest number of trades took place. Do the same considering trades for each exchange separately.
+
+The input file is passed as a program argument.
---
+++ src/Main.kt
@@ -0,0 +1,68 @@
+import java.io.File
+import java.time.LocalTime
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+fun main(args: Array<String>) {
+
+    val path = args[0]
+    val generalQueue = LinkedList<Long>()
+    val queues = HashMap<String, LinkedList<Long>>()
+    var maxForAllExchanges = 0
+    var maxForAllExchangesTime = 0L
+    val maxForEachExchange = HashMap<String, Pair<Int, Long>>()
+    val oneSecond = TimeUnit.SECONDS.toNanos(1)
+
+    File(path).useLines {
+        val withoutHeader = it.drop(1)
+        withoutHeader.forEach {
+            val (timeString, _, _, exchange) = it.split(",")
+            val time = LocalTime.parse(timeString).toNanoOfDay()
+            if (generalQueue.isEmpty() || time - generalQueue.peek() < oneSecond) {
+                generalQueue.add(time)
+            } else {
+                if (maxForAllExchanges < generalQueue.size) {
+                    maxForAllExchanges = generalQueue.size
+                    maxForAllExchangesTime = generalQueue.pop()
+                }
+                while (generalQueue.isNotEmpty() && time - generalQueue.peek() >= oneSecond) {
+                    generalQueue.pop()
+                }
+                generalQueue.add(time)
+            }
+
+            val currentQueue = queues.getOrPut(exchange) { LinkedList() }
+            if (currentQueue.isEmpty() || time - currentQueue.peek() < oneSecond) {
+                currentQueue.add(time)
+            } else {
+                val currentMax = maxForEachExchange[exchange]
+                if (currentMax == null || currentMax.first < currentQueue.size) {
+                    maxForEachExchange[exchange] = currentQueue.size to currentQueue.pop()
+                }
+                while (currentQueue.isNotEmpty() && time - currentQueue.peek() >= oneSecond) {
+                    currentQueue.pop()
+                }
+                currentQueue.add(time)
+            }
+        }
+    }
+
+    if (maxForAllExchanges < generalQueue.size) {
+        maxForAllExchanges = generalQueue.size
+        maxForAllExchangesTime = generalQueue.pop()
+    }
+
+    queues.forEach { (exchange, queue) ->
+        val currentMax = maxForEachExchange[exchange]
+        if (currentMax == null || currentMax.first < queue.size) {
+            maxForEachExchange[exchange] = queue.size to queue.pop()
+        }
+    }
+
+    println("Total max: $maxForAllExchanges at ${LocalTime.ofNanoOfDay(maxForAllExchangesTime)} - "
+            + "${LocalTime.ofNanoOfDay(maxForAllExchangesTime + oneSecond)}")
+    maxForEachExchange.forEach {
+        println("Max for exchange ${it.key}: ${it.value.first} at ${LocalTime.ofNanoOfDay(it.value.second)} - "
+                + "${LocalTime.ofNanoOfDay(it.value.second + oneSecond)})")
+    }
+}
